c6bcaebfef91b9ecb1592f965e347c5cf33b895f
Kobe Michiels <kobe.michiels@outlook.com>
2024-04-18 11:16:40 +0200

    Adding Carry Lookahead Adder

--- final-project/final-project.srcs/sources_1/new/carry_lookahead_adder.v
+++ final-project/final-project.srcs/sources_1/new/carry_lookahead_adder.v
@@ -31,11 +31,20 @@ module carry_lookahead_adder#(
     );
     genvar i;
     
+    wire[ADDER_WIDTH-1:0] wP, wG, wCarry;
+    wire [ADDER_WIDTH-1:0] wSum;
+    
+    pf_adder pf_adder_inst(.iA(iA[0]), .iB(iB[0]), .iCarry(iCarry), .oSum(wSum[0]), .oP(wP[0]), .oG(wG[0]));
+    cll_block_array #(.ADDER_WIDTH(ADDER_WIDTH)) cll_block_array_inst(.iP(wP), .iG(wG), .iCarry(iCarry), .oCarry(wCarry));
+    
     generate
-        for(i = 0; i<ADDER_WIDTH; i=i+1)
+        for(i = 1; i<ADDER_WIDTH; i=i+1)
         begin
-            pf_adder pf_adder_inst(.iA(iA[i]), .iB(iB[i]), .iCarry());
+            pf_adder pf_adder_inst(.iA(iA[i]), .iB(iB[i]), .iCarry(wCarry[i-1]), .oSum(wSum[i]), .oP(wP[i]), .oG(wG[i]));
         end
     endgenerate 
+    
+    assign oSum = wSum;
+    assign oCarry = wCarry[ADDER_WIDTH-1];
     
 endmodule
